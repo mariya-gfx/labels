f66236cd0f312dd67d73e93c241053799e2e9bbb
alightwing <34476179+alightwing@users.noreply.github.com>
2021-08-02 15:49:01 +0100

    Update labels.py to most recent common version (#14)
    Co-authored-by: Martin Packman <martin.packman@visual-meaning.com>

--- labels.py
+++ labels.py
@@ -3,13 +3,21 @@
 """Turn json ai semantic export into a transform."""
 
 import argparse
+import base64
+import hashlib
+import itertools
 import json
 import pprint
 import sys
 
 
+PATH_CHAR = '\xb7'
+
+
 def deriveColor(col):
     """Create CSS color from i9r json."""
+    if not col:
+        return None
     width = 2
     if all(map((lambda n: n >> 4 == n & 15), col.values())):
         col = {k: v & 15 for k, v in col.items()}
@@ -17,76 +25,171 @@ def deriveColor(col):
     return '#{red:0{w}x}{green:0{w}x}{blue:0{w}x}'.format(**col, w=width)
 
 
+def fixColor(string):
+    if string is None:
+        return '#fff'
+    return '#' + string.lstrip('#')
+
+
+def deriveClass(url):
+    if url == 'activity':
+        return 'vm:HE/Activity'
+    return url.replace('https:', 'http:')
+
+
+def deriveHashIdent(obj):
+    if obj['classFor'] == 'http://www.w3.org/ns/org#OrganisationalUnit':
+        raw = obj['note']['org_unit_id']
+    else:
+        raw = str(obj['n']) + deriveIdent(obj['path'])
+    hash_prefix = hashlib.sha256(raw.encode('utf-8')).digest()[:6]
+    return base64.urlsafe_b64encode(hash_prefix).decode('ascii')
+
+
 def deriveIdent(path):
-    return '-'.join(path.split('/')[1:]).strip()
+    return '-'.join(path.split(PATH_CHAR)[1:]).strip()
 
 
 def deriveNote(contents):
     # TODO: Parse a bit more like TOML rules, and fail sensibly
-    entries = (line.partition('=') for line in contents.strip().splitlines())
+    lines = (
+        contents.strip()
+        .replace('\u201c', '"')
+        .replace('\u201d', '"')
+        .replace('" ', '"\n')
+        .replace('org_unit_id:', 'org_unit_id =')
+        .replace(' class:', '\nclass =')
+        .replace('fullname =', 'fullName =')
+        .splitlines())
+    entries = (line.partition('=') for line in lines)
     return {k.strip(): v.strip().strip('"') for k, _, v in entries}
 
 
 def deriveQcon(path):
-    parts = path.rsplit('/', 2)[1:]
-    if len(parts) == 1:
-        return parts[0].strip()
-    return ' '.join([parts[0].strip().rstrip('s'), parts[1].strip()])
+    parts = path.split(PATH_CHAR)
+    if not parts or len(parts) < 2:
+        return ''
+    off = 1 if len(parts) < 3 else 2
+    return ': '.join(p.strip() for p in parts[off:])
+
+
+def deriveAcon(obj):
+    if 'parent' not in obj:
+        return ''
+    if obj['classFor'] == 'http://www.w3.org/ns/org#OrganisationalUnit':
+        return 'orgunit-{:d}'.format(int(obj['note']['org_unit_id']))
+    parent = obj['parent']
+    while True:
+        anchor = parent.get('anchorName')
+        if anchor != 'Management':
+            break
+        parent = parent['parent']
+    local = obj['note'].get('fullName') or ' '.join(obj['text'].split())
+    if anchor:
+        return ': '.join([anchor, local])
+    return local
 
 
 def deriveText(string):
-    return '\n'.join(line.strip() for line in string.split('\r'))
+    lines = string.strip().replace('\u0003', ' ').split('\r')
+    return '\n'.join(line.strip() for line in lines)
 
 
-def map_layers(layers, omit):
+def deriveLocation(scaler, loc):
+    if loc is None:
+        return None
+    if isinstance(loc, list):
+        result = pair(scaler.latlng_bounds(loc))
+    elif isinstance(loc, dict):
+        result = scaler.path(loc)
+    else:
+        result = [scaler.path(p) for d in loc for p in d]
+        if not result:
+            return None
+    return repr(result).replace(' ', '')
+
+
+def pair(alist):
+    return [[alist[i-1], alist[i]] for i in range(1, len(alist), 2)]
+
+
+def json_str(obj):
+    """Dump as json str in most compact representation"""
+    return json.dumps(obj, separators=(',', ':'))
+
+
+def map_layers(layers, name_for_layer, omit):
     for layer in layers:
         name = layer['name']
         if name.startswith(('~', '-', '-~')) and name not in omit:
-            yield dict(depth=0, uid=name, layer=name, **layer)
+            layer_name = '-'.join(name.lstrip('-~').split(' · ')[0].strip().lower().split())
+            if name_for_layer is not None:
+                layer_name = name_for_layer + '@' + layer_name
+            yield dict(depth=0, uid=name, layer=layer_name, **layer)
 
 
-def map_labels(labels, parent):
+def map_labels(labels, parent, counter):
     for label in labels:
-        if label['name']['contents'] == parent.get('contents'):
+        contents = label['name']['contents']
+        if contents == parent.get('contents') or not contents.strip():
+            print ('DUPE', contents.replace('\r', ' '), file=sys.stderr)
             continue  # Skip duplicated immediate child labels
+        note = deriveNote(label['name'].pop('note', ''))
         yield dict(
             depth=parent['depth'] + 1,
             children=label.get('children', ()),
             layer=parent['layer'],
             parent=parent,
+            area=label.get('area'),
+            dataLocation=label.get('dataCollections', {}).values(),
+            visibleBounds=label.get('visibleBounds'),
             path=label['path'],
             uid=label['uID'],
+            n=next(counter),
+            text=deriveText(label['name'].pop('contents')),
+            anchorName=note.pop('anchorName', parent.get('anchorName', '')),
+            classFor=deriveClass(note.pop('class', parent.get('class', 'vm:HE/Activity'))),
+            note=note,
             **label['name'],
         )
 
 
 def iter_labels(layers, scaler):
+    counter = itertools.count()
     stack = list(layers)
     stack.reverse()
     while stack:
         o = stack.pop()
-        stack.extend(map_labels(reversed(o['children']), o))
+        stack.extend(map_labels(reversed(o['children']), o, counter))
         print(
             '{indent}{name}'.format(
                 indent=o['depth'] * '  ',
-                name=o.get('name') or o.get('contents')),
+                name=o.get('name') or o['text'].replace('\n', ' ')),
             file=sys.stderr)
-        if o.get('type') or o.get('hidden'):
+        if o.get('type'):
             continue
-        note = deriveNote(o.get('note', ''))
-        yield dict(
-            bounds=repr(scaler.latlng_bounds(o['bounds'])),
-            centrePoint=repr(scaler.latlng(o['centrePoint'])),
-            boxColor=note.get('background', '#fff'),
-            characterColor=deriveColor(o['characterColour']),
-            fontSize=repr(scaler.distance(o['fontSize'])),
-            contents=deriveText(o['contents']),
-            ident=deriveIdent(o['path']),
-            layer=o['layer'].strip('-~').strip(),
-            qcontents=deriveQcon(o['path']),
-            typeface=o['typeface'],
-            # TODO: matrix, opacity, area
+        row = dict(
+            ident=deriveHashIdent(o),
+            qcontents=deriveAcon(o),
+            layer=o['layer'],
+            pqcontents=deriveAcon(o['parent']),
+            area=deriveLocation(scaler, o['area'] or o['visibleBounds']),
+            dataLocation=deriveLocation(scaler, o['dataLocation']),
+            type=o['classFor'],
         )
+        if not o.get('hidden'):
+            display = dict(
+                box=scaler.latlng_bounds(o['bounds']),
+                background=fixColor(o['note'].get('background')),
+                color=deriveColor(o['characterColour']),
+                text=o['text'],
+                fontFamily=o['typeface'],
+                fontSize=scaler.distance(o['fontSize']),
+                # TODO: matrix, opacity
+            )
+            row['display'] = json_str(display)
+            row['geoPoint'] = json_str(scaler.latlng(o['centrePoint']))
+        yield row
 
 
 class Scaler:
@@ -108,7 +211,7 @@ class Scaler:
     def from_rect(cls, x1, y1, x2, y2):
         scale_x = abs(cls.SIZE / (x2 - x1))
         scale_y = abs(cls.SIZE / (y2 - y1))
-        assert scale_x == scale_y
+        #assert scale_x == scale_y
         return cls(x1, y1, scale_x)
 
     @classmethod
@@ -117,6 +220,13 @@ class Scaler:
 
     def _to(self, n):
         return round(n * self.scale, self.ROUND)
+
+    def path(self, o):
+        if o.get('type') is not None:
+            return [self.latlng(segment['anchor']) for segment in o['points']]
+        if o.get('areaType') == 'pathPoints':
+            return [self.latlng(segment['anchor']) for segment in o['points']]
+        raise NotImplementedError(o)
 
     def distance(self, n):
         return self._to(n)
@@ -135,45 +245,31 @@ class Scaler:
         ]
 
 
-def to_transform(data, omit):
+def to_transform(data, name_for_layer, omit):
     scaler = Scaler.from_artboard(data['artboard'])
-    layers = map_layers(data['layers'], omit)
+    layers = map_layers(data['layers'], name_for_layer, omit)
     rows = list(iter_labels(layers, scaler))
     # TODO: merge labels across views
     return {
         'data': rows,
         'lets': {
-            # TODO: Clarify label ident as per-layer but thing ident as common
-            'iri': 'vm:label-{row[ident].as_slug}',
+            'iri': 'vm:l{row[layer].as_text}-{row[ident].as_text}',
             'layer': '{row[layer].as_text}',
-            'name': '{row[contents].as_text}',
-            'nname': '{row[qcontents].as_text}',
+            'content': '{row[qcontents].as_text}',
+            'maybe_for': 'vm:_thing_{row[qcontents].as_slug}',
         },
         'queries': {
-            'for': '''
-                select ?s where {
-                    ?s vm:name ?o .
-                    filter (?o in (?name, ?nname))
-                }
-            ''',
-            'view': '''
-                select ?s where {
-                    ?s rdf:type vm:View .
-                    ?s vm:comment ?layer .
-                }
-            ''',
+            'for': (
+                'select ?maybe_for where {'
+                '  { ?maybe_for ?p ?o } union { ?s ?p ?maybe_for }'
+                '} limit 1'),
         },
         'triples': [
             ('{iri}', 'rdf:type', 'vm:Label'),
-            ('{iri}', 'vm:fontSize', '{row[fontSize]}'),
-            ('{iri}', 'vm:fontFamily', '{row[typeface]}'),
-            ('{iri}', 'vm:description', '{name}'),
-            ('{iri}', 'vm:atGeoPoint', '{row[centrePoint]}'),
-            ('{iri}', 'vm:fontColor', '{row[characterColor]}'),
-            ('{iri}', 'vm:boxBounds', '{row[bounds]}'),
-            ('{iri}', 'vm:boxColor', '{row[boxColor]}'),
+            ('{iri}', 'vm:atGeoPoint', '{row[geoPoint].as_text}'),
+            ('{iri}', 'vm:display', '{row[display].as_text}'),
             ('{iri}', 'vm:forThing', '{for}'),
-            ('{iri}', 'vm:forView', '{view}'),
+            ('{iri}', 'vm:forLayer', '{layer}'),
         ],
     }
 
@@ -181,6 +277,7 @@ def to_transform(data, omit):
 def main(argv):
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--encoding', default='utf-8')
+    parser.add_argument('--name-layer')
     parser.add_argument('--omit-layer', action='append')
     parser.add_argument('--output')
     parser.add_argument('input')
